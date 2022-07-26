b958b79da1b7e7b2fb0e9b87ab6c0fe7978a23ae
Tortitas <victorgf2011@gmail.com>
2022-07-26 10:40:52 +0200

    loop refactor, name and custom interval

--- main.js
+++ main.js
@@ -2,10 +2,13 @@ const { app, BrowserView, BrowserWindow } = require('electron')
 const fs = require('fs')
 
 const createWindow = () => {
-  const win = new BrowserWindow({ width: 800, height: 600 })
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    backgroundColor: '#121212',
+  })
   return win
 }
-
 const createView = (window, url) => {
   const view = new BrowserView()
 
@@ -28,36 +31,71 @@ const createView = (window, url) => {
   return view
 }
 
+let steps
+let defaultInterval
+let index
+let stepViews
+let window
+
 app.whenReady().then(() => {
   const config = JSON.parse(fs.readFileSync('config.json'))
 
-  const url = config.urls
-  const interval = config.interval
+  steps = config.steps
+  defaultInterval = config.interval
 
-  const window = createWindow()
-  let index = 0
-  let urlView = []
+  window = createWindow()
+  index = 0
+  stepViews = []
 
-  for (let i = 0; i < url.length; i++) {
-    urlView[i] = createView(window, url[i])
+  for (let i = 0; i < steps.length; i++) {
+    stepViews[i] = createView(window, steps[i].url)
+
+    stepViews[i].webContents.on('dom-ready', () => {
+      setTimeout(() => {
+        stepViews[i].webContents.executeJavaScript(`
+          $p = document.createElement('p')
+          document.body.appendChild($p)
+          $p.innerHTML = '${steps[i].name || 'UNKNOWN'}'
+          $p.style.position = "absolute"
+          $p.style.display = "block"
+          $p.style.fontSize = "3em"
+          $p.style.fontWeight = "bold"
+          $p.style.color = "white"
+          $p.style.opacity = "50%"
+          $p.style.top = "20px"
+          $p.style.right = "10px"
+        `)
+      }, 0)
+    })
   }
 
-  window.setBrowserView(urlView[0])
-  index++
+  main()
+})
 
-  setInterval(() => {
-    window.setBrowserView(urlView[index])
-    urlView[index].setBounds({
-      x: 0,
-      y: 0,
-      width: window.getContentBounds().width,
-      height: window.getContentBounds().height,
-    })
+async function main() {
+  while (true) {
+    window.setBrowserView(stepViews[index])
 
-    if (index == url.length - 1) {
+    resizeView(window, stepViews[index])
+
+    await sleep(steps[index].interval || defaultInterval)
+
+    if (index == steps.length - 1) {
       index = 0
     } else {
       index++
     }
-  }, interval)
-})
+  }
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+function resizeView(window, view) {
+  view.setBounds({
+    x: 0,
+    y: 0,
+    width: window.getContentBounds().width,
+    height: window.getContentBounds().height,
+  })
+}
